833483c8574c31073741ba09bb7810c74fae730b
Leonid <shcherban88@gmail.com>
2020-10-05 01:41:35 +0300

    popup,js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,21 @@
-
     const popup = document.querySelector('.popup');
     const popupToggle = document.querySelector('.profile__edit-button');
     const popupClose = document.querySelector('.popup__close-icon');
-    const nameInput = document.querySelector('.popup__name-input');
-    const jobInput = document.querySelector('.popup__job-input');
+    const nameInput = document.querySelector('.popup_name');
+    const jobInput = document.querySelector('.popup_job');
     const formElement = document.querySelector('.popup__form');
     const profileName = document.querySelector('.profile__name');
     const profileJob = document.querySelector('.profile__job');
 
-
     function openPopUp() {
-        popup.classList.add('popup_open')
+        popup.classList.add('popup_open');
         nameInput.value = profileName.textContent;
         jobInput.value =  profileJob.textContent;
         
     }
 
     function closePopUp() {
-        popup.classList.remove('popup_open')
+        popup.classList.remove('popup_open');
     }
         
 
@@ -27,7 +25,7 @@
         profileName.textContent = nameInput.value;
         profileJob.textContent = jobInput.value;
 
-    closePopUp()
+    closePopUp();
     }
         formElement.addEventListener('submit',formSubmitHandler); 
         popupToggle.addEventListener('click',openPopUp); 
